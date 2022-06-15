16673cef44b99e1e91135c61a976edc1d1373d44
mighty_volcano <72347597+Harshkumar77@users.noreply.github.com>
2022-06-16 01:25:03 +0530

    added react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,3 @@
-import { useEffect, useState } from "react"
-import axios from "axios"
-
-function App() {
-  useEffect(() => {
-    axios.get("/api").then((_) => console.log(_.data))
-    return () => {}
-  }, [])
-
-  return <h1 className="text-5xl">Hello Starter</h1>
-}
+function App() {}
 
 export default App
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { Route } from "react-router-dom"
+import { Routes } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
+import App from "./App"
+import "./index.css"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="register" element={<>Register</>} />
+          <Route path="login" element={<>login</>} />
+          <Route path="*" element={<>Not found</>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
